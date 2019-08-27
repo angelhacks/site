77650b0dd9f66d0d9a7f4dcc7d34b89fb619bb61
Claire Wang <clara32356@gmail.com>
2019-08-27 01:05:40 -0700

    shawee

--- components/sponsors.js
+++ components/sponsors.js
@@ -51,6 +51,12 @@ const data = {
       link: 'https://sketchapp.com/',
       image: 
         'https://angelhacks.org/static/sketch-logo.png'
+    },
+    {
+      name: 'Shawee',
+      link: 'https://shawee.io/',
+      image: 
+        'https://angelhacks.org/static/shawee-logo.png'
     }
   ]
 }
