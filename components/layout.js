7bb874125f06cb0898eb09566d6675b1011b72d4
Matthew Stanciu <mattbstanciu@gmail.com>
2019-08-27 08:32:19 -0400

    Remove link to Lachlan's website

--- components/layout.js
+++ components/layout.js
@@ -134,12 +134,7 @@ export default props => {
               a: { fontSize: 1 }
             }}
           >
-            <A
-              href="https://lachlanjc.me"
-              target="_blank"
-              variant="styles.navitem"
-              sx={{ color: 'inherit', fontSize: 0 }}
-            >
+            <Text sx={{ fontFamily: 'heading', fontSize: 0, color: 'inherit', variant="styles.navitem" }}>
               AngelHacks, event run by Ad Astra Hack Club
             </A>
             <Text
